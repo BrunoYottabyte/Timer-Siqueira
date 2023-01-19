47b7315b8faf30b5dcc73ec0f0395cc74c62d5f9
Bruno Siqueira <projetointegrador792@gmail.com>
2023-01-19 02:50:38 -0300

    circle svg

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { differenceInSeconds } from "date-fns";
 import { CountdownContainer, Separator } from "./styles";
 import { CyclesContext } from "../../../../contexts/CyclesContext";
@@ -11,7 +11,7 @@ const Countdown = () => {
     amountSecondsPassed,
     setSecondsPassed,
   } = useContext(CyclesContext);
-
+  const [percent, setPercent] = useState(100);
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
@@ -22,12 +22,17 @@ const Countdown = () => {
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
 
+  const progressCircle = useRef(null);
+  const radius = progressCircle.current?.r.baseVal.value;
+  const circumference = radius * 2 * Math.PI;
+
   useEffect(() => {
     if (activeCycle) document.title = `${minutes}:${seconds}`;
   }, [minutes, seconds, activeCycle]);
 
   useEffect(() => {
     let idInterval: number;
+    setPercent((currentSeconds / totalSeconds) * 100);
     if (activeCycle) {
       idInterval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
@@ -39,6 +44,7 @@ const Countdown = () => {
           markCurrentCycleAsFinished();
           setSecondsPassed(totalSeconds);
           clearInterval(idInterval);
+          setPercent(0);
         } else {
           setSecondsPassed(secondsDifference);
         }
@@ -50,14 +56,52 @@ const Countdown = () => {
     };
   }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished]);
 
+  useEffect(() => {
+    if (!progressCircle.current) return;
+    progressCircle.current.style.strokeDasharray = circumference;
+    console.log(percent);
+    setProgress(-percent);
+  }, [percent]);
+
+  function setProgress(percent: number) {
+    if (!progressCircle.current) return;
+    progressCircle.current.style.strokeDashoffset =
+      circumference - (percent / 100) * circumference;
+  }
+
   return (
-    <CountdownContainer>
-      <span>{minutes[0]}</span>
-      <span>{minutes[1]}</span>
-      <Separator>:</Separator>
-      <span>{seconds[0]}</span>
-      <span>{seconds[1]}</span>
-    </CountdownContainer>
+    <>
+      <CountdownContainer>
+        <span>{minutes[0]}</span>
+        <span>{minutes[1]}</span>
+        <Separator>:</Separator>
+        <span>{seconds[0]}</span>
+        <span>{seconds[1]}</span>
+      </CountdownContainer>
+      <svg width="250" height="250">
+        <circle
+          cx="125"
+          cy="60"
+          r="50"
+          className="track"
+          fill="transparent"
+          stroke="#ccc"
+          strokeWidth={1}
+          rotate="-90deg"
+        />
+        <circle
+          ref={progressCircle}
+          cx="125"
+          cy="60"
+          r="50"
+          strokeWidth={3}
+          className="progress"
+          transform="rotate(-90,125,60)"
+          fill="transparent"
+          stroke="red"
+        />
+      </svg>
+    </>
   );
 };
 
